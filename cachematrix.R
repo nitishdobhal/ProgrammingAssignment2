e2517a9c744808d855ee08c57881f6088f0c9ee6
Nitish <nitishdobhal@gmail.com>
2016-11-09 18:45:10 -0800

    My commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## First function takes a matrix and creates a list with 
+## 4 local functions to operate on the matrix object.
 
-## Write a short comment describing this function
+## set function used to initialze the local varible, get function to return the originl matrix
+## setinv to store inv into cache and getinv to get the inverse stored.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv<-NULL
+  set<-function (y) {
+      x<<-y
+      inv<<-NULL
+  }
+  get<-function() x
+  setinv<-function(t) inv<<-t
+  getinv<-function()inv
+  list(set=set,get=get,setinv=setinv,getinv=getinv)
+  
 }
 
 
-## Write a short comment describing this function
+## Function checks if the cache has the stored value
+## if not then it calculates the inverse and stores it in the cache variables using the previous function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inv<-x$getinv()
+  if(!is.null(inv)){
+      message("Getting from Cache")
+      return(m)
+  }
+  data<-x$get()
+  inv<-solve(data,...)
+  x$setinv(inv)
+  inv
 }
